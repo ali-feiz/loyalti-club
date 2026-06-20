e87932a2c9af158753e45305425b89b7b2aaaec8
ali feizabadi <alirezafeizabadi1379@email.com>
2026-06-20 11:00:31 +0330

    change api core and env file

--- src/core/api/core.ts
+++ src/core/api/core.ts
@@ -1,7 +1,20 @@
 import axios from "axios";
 
+const scheme = import.meta.env.VITE_SSL === "true" ? "https://" : "http://";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: scheme + import.meta.env.VITE_API_URL + "/api",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(error.response);
+  },
+);
 
 export default api;
